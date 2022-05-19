a09c5ef1d416e96eed602797cbd2d3dba61c5927
Sasha Kramer <sashasemail@gmail.com>
2022-05-19 15:11:27 -0400

    add hooks & variables

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -2,14 +2,32 @@ import React, { useState, useEffect } from 'react'
 
 
 export default function Carousel(props){
-    let { slides } = props
+    const { slides } = props
+
+    const [activeSlide, setActiveSlide] = useState(0)
+
+    useEffect(() => {
+        let interval = window.setInterval(() => {
+            nextSlide()
+        }, 10000)
+
+        return () => window.clearInterval(interval)
+    }, [])
+
+    function nextSlide(){
+
+    }
+    function previousSlide(){
+
+    }
+
     return (
-        <section role="carousel">
+        <section role="region" aria-label="Image Carousel">
             {/* carousel */}
             <div className="slides-container">
                 {slides.map((slide, index) => {
                     return (
-                        <div key={`carousel-slide-${index}`}>
+                        <div className="carousel-slide" key={`carousel-slide-${index}`}>
                             slide {index}
                         </div>
                     )
@@ -17,16 +35,28 @@ export default function Carousel(props){
             </div>
 
             <div className="previous-slide">
-                <button className="previous-slide-button" aria-label="Previous Slide">Previous</button>
+                <button
+                    className="previous-slide-button" 
+                    aria-label="Previous Slide"
+                    onClick={() => previousSlide()}
+                >
+                    Previous
+                </button>
             </div>
             <div className="next-slide">
-                <button className="next-slide-button" aria-label="Next Slide">Next</button>
+                <button
+                    className="next-slide-button" 
+                    aria-label="Next Slide"
+                    onClick={() => nextSlide()}
+                >
+                    Next
+                </button>
             </div>
 
-            <div className="carousel-indicators">
+            <div className="slide-indicators">
                 {slides.map((slide, index) => {
                     return (
-                        <div>
+                        <div key={`carousel-slide-indicator-${index}`}>
                             <button className="slide-indicator-button" aria-label={`Go To Slide ${index}`}>
                                 Go To Slide {index}
                             </button>
@@ -34,6 +64,8 @@ export default function Carousel(props){
                     )
                 })}
             </div>
+
+            {/* TODO: Add pause autoplay button, at least for screen readers */}
         </section>
     )
 }
